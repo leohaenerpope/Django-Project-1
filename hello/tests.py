fc1b8e8d56d0a4d0476f26541bab32195bee297c
Leo Haener-Pope <leohaenerpope@u.boisestate.edu>
2026-01-28 15:21:57 -0700

    Old python version to work with tests

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class WebsiteTest(TestCase):
+    def test_hello_world_access(self):
+        response = self.client.get(reverse('hello_there'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hello/hello_there.html')
+        self.assertTemplateNotUsed(response, 'hello/hello_there1.html')
--- web_project/settings.py
+++ web_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
